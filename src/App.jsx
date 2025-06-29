611c3e5dfa462ead411d7cdd04d153be8cc96882
Keaton Murray <murraykeaton422@gmail.com>
2025-06-29 01:31:06 -0400

    modified scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './layout/Layout';
 import Home from './pages/Home'
 
 const App = () => {
+  useEffect(() => {
+  const hash = window.location.hash;
+  if (hash) {
+    const el = document.querySelector(hash);
+    if (el) {
+      setTimeout(() => {
+        el.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  }
+}, []);
   return (
     <Routes>
       <Route path="/" element={<Layout><Home/></Layout>}>
