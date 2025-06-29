7a0940822f9a507b46d83069f9cb605f3c37517a
Keaton Murray <murraykeaton422@gmail.com>
2025-06-29 03:27:47 -0400

    removed google font cdn and installed npm

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "@fontsource/poppins";     
+import "@fontsource/roboto";
+import "@fontsource/open-sans";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 import './assets/css/style.css';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
